3998627e71b41b271f971257fa956d121c30f651
JBUinfo <37612885+JBUinfo@users.noreply.github.com>
2021-11-06 12:58:34 +0100

    V.2 Simpler and cleaner

--- SyncReadFilesMongoNode.js
+++ SyncReadFilesMongoNode.js
@@ -1,9 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const LineByLineReader = require('line-by-line');
-const PATHPADRE = 'D:/XXX';
-//emails
-const re = new RegExp(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i);
+const PARENTPATH = 'D:/XXX';
 
 //DB
 const mongoose = require('mongoose');
@@ -12,10 +10,9 @@ mongoose.set('debug', false);
 mongoose.connect('mongodb://localhost/XXX', {useNewUrlParser: true,   useUnifiedTopology: true});
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
-const Accounts = mongoose.model('accounts',new Schema ({
+const FileLineModel = mongoose.model('FileLineModel',new Schema ({
   file: String,
-  user: String,
-  pass: String
+  lineDB: String
 },{versionKey: false}));
 
 //Return every .txt file
@@ -32,7 +29,7 @@ let walkSync = function(dir, filelist) {
   });
   return filelist;
 };
-walkSync=walkSync(PATHPADRE,null);
+walkSync=walkSync(PARENTPATH,null);
 
 function sleep(ms){
   return new Promise(resolve=>{
@@ -47,70 +44,42 @@ const test = class {
     try {
       let status; //flag to make it sync
       let status2; //flag to pause or resume the stream
-      let a;//interval
+      let intervl;//interval
       let count = 0 ;//count how many seconds waits when inserts on mongoDB
       let lr; //data stream
-      let user;//substring
-      let pass;//substring
-      let arrray = [];
+      let tempArray = [];
+      
       //each file
       for (let i = 0; i < walkSync.length; i++) {
         lr = new LineByLineReader(walkSync[i]);
         status = true;
-        arrray = [];
+        tempArray = [];
         console.log(i + ' - ' + walkSync[i]);
+        
         //each line
         lr.on('line', function (line) {
           count = 0;
           status2 = true;
-          user = '';
-          pass = '';
-          if (line.includes("@")) {
-            if (line.includes(":")) {
-              user = line.substring(line.indexOf(":") , line.indexOf(''));
-              pass = line.substring(line.indexOf(":") + 1);
-            } else if (line.includes(";")) {
-              user = line.substring(line.indexOf(";") , line.indexOf(''));
-              pass = line.substring(line.indexOf(";") + 1);
-            } else if (line.includes("|")) {
-              user = line.substring(line.indexOf("|") , line.indexOf(''));
-              pass = line.substring(line.indexOf("|") + 1);
-            } else if (line.includes("||")) {
-              user = line.substring(line.indexOf("||") , line.indexOf(''));
-              pass = line.substring(line.indexOf("||") + 1);
-            } else if (line.includes("\t")) {
-              user = line.substring(line.indexOf("\t") , line.indexOf(''));
-              pass = line.substring(line.indexOf("\t") + 1);
-            } else if (line.includes(" ")) {
-              user = line.substring(line.indexOf(" ") , line.indexOf(''));
-              pass = line.substring(line.indexOf(" ") + 1);
-            } else if (line.includes("  ")) {
-              user = line.substring(line.indexOf("  ") , line.indexOf(''));
-              pass = line.substring(line.indexOf("  ") + 1);
-            }
-            if (pass != '' & re.test(String(user).toLowerCase())) {
-              arrray.push({file:walkSync[i],user: user,pass: pass});//insert on array
-              if (arrray.length == 100000) {//every 100.000 lines, insert
-                lr.pause();//pause streaming
-                Accounts.insertMany(arrray, {ordered:false, w:0}, function (err, docs) {//insert on mongoDB
-                  status2 = false;
-                });
-                a = setInterval(function(){//every second verify if has inserted all on mongoDB
-                  count++;
-                  console.log('pause'+count);
-                  if (!status2) {
-                    lr.resume();
-                    clearInterval(a);
-                    arrray = [];
-                    console.log('-End pause-');
-                  }
-                }, 1000);
+          tempArray.push({file:walkSync[i],lineDB: line});//insert on array
+          if (tempArray.length == 100000) {//every 100.000 lines, insert
+            lr.pause();//pause streaming
+            FileLineModel.insertMany(tempArray, {ordered:false, w:0}, function (err, docs) {//insert on mongoDB
+              status2 = false;
+            });
+            intervl = setInterval(function(){//every second verify if has inserted all on mongoDB
+              count++;
+              console.log('pause'+count);
+              if (!status2) {
+                lr.resume();
+                clearInterval(intervl);
+                tempArray = [];
+                console.log('-End pause-');
               }
-            }
+            }, 1000);
           }
         })
-        lr.on('end', function () {
-          Accounts.insertMany(arrray, {ordered:false, w:0}, function (err, docs) {//insert on mongoDB
+        lr.on('end', function () {//file finished 
+          FileLineModel.insertMany(tempArray, {ordered:false, w:0}, function (err, docs) {//insert on mongoDB
             status = false;
           });
           console.log(walkSync[i]+' CLOSED');
